1fac747f407222a31af6546130e8922a61287cdf
franko <franko@akr.club>
2017-04-04 18:36:08 -0400

    update

--- edb-functionality.php
+++ edb-functionality.php
@@ -21,7 +21,7 @@ function edb_woocommerce_support() {
 }
 add_filter('woocommerce_get_price', 'edb_return_custom_price', 2); 
 function edb_return_custom_price( $product, $price) {
-  $base_price = rwmb_meta('edb_base_price', null, $post_id);
+  $base_price = rwmb_meta('edb_base_price', null, $product->id);
   return $base_price;
 }
 //     // Grab the product id
