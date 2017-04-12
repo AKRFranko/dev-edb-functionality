6fd290d1116c91b755118647b6c41163f2f64299
franko <franko@akr.club>
2017-04-12 13:38:35 -0400

    update

--- edb-functionality.php
+++ edb-functionality.php
@@ -30,7 +30,7 @@ function edb_return_custom_price(  $price,$product) {
     $base_price = rwmb_meta('edb_base_price', null, $product->id);  
   }
   if($parent_id == 201){
-    var_dump($price, $product 1);
+    var_dump($price, $product );
   }
   
   $gids = rwmb_meta('edb_group_ids', null, $product->id);
