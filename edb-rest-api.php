336d0022f987f6349ee95bb96ee7eb9b72f2908a
franko <franko@akr.club>
2017-08-09 16:05:18 -0400

    update

--- edb-rest-api.php
+++ edb-rest-api.php
@@ -16,7 +16,7 @@ function edb_rest_get_thumbnail_url($post){
 function edb_rest_get_thumbnail_colors($post){
     
   // $colors = get_post_meta(get_post_thumbnail_id( $post['id'] ),'color_palette_hex');
-   return 'YES!!' //$colors;
+   return 'YES!!'; //$colors;
 }
 
 
