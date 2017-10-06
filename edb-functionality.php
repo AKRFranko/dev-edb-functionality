bc1242c23ebd8a7e63223ba9327efd53927e30bc
franko <franko@akr.club>
2017-10-06 16:35:44 -0400

    update

--- edb-functionality.php
+++ edb-functionality.php
@@ -124,18 +124,20 @@ remove_filter('the_content', 'wpautop');
 
 
 
+// // Load necessary admin files.
+require_once ABSPATH . 'wp-admin/includes/template.php';
+require_once ABSPATH . 'wp-admin/includes/post.php';
+// // Load plugin main class.
+require_once dirname( __FILE__ ) . '/class-mb-rest-api.php';
+$mb_rest_api = new MB_Rest_API;
+add_action( 'rest_api_init', array( $mb_rest_api, 'init' ) );
+
 include 'edb-post-types.php';
 
 
-// // Load necessary admin files.
-// require_once ABSPATH . 'wp-admin/includes/template.php';
-// require_once ABSPATH . 'wp-admin/includes/post.php';
-// // Load plugin main class.
-// require_once dirname( __FILE__ ) . '/class-mb-rest-api.php';
-// $mb_rest_api = new MB_Rest_API;
-// add_action( 'rest_api_init', array( $mb_rest_api, 'init' ) );
 
-include 'edb-mb-rest-api.php';
+
+// include 'edb-mb-rest-api.php';
 
 include 'edb-rest-api.php';
 include 'edb-wc-rest-api.php';
