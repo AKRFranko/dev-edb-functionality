390f7b88d4750b347e647366bb0227c90b292ec6
franko <franko@akr.club>
2017-10-20 12:23:00 -0400

    update

--- edb-rest-api.php
+++ edb-rest-api.php
@@ -29,6 +29,9 @@ function edb_rest_get_product_metabox($post){
     if($post['meta_data']){
       foreach($post['meta_data'] as $k  => $v){
         $meta[$v->key]  =  $v->value;
+        if($k === 'db_wireframe'){
+          $meta[$v->key] = wp_get_attachment_image_src($v->value);
+        }
       }
     }
     return $meta;
