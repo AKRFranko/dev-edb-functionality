84ad59d65274b2035b98765d86fb918994165285
franko <franko@akr.club>
2017-12-14 15:39:40 -0500

    update

--- edb-rest-api.php
+++ edb-rest-api.php
@@ -30,6 +30,8 @@ function edb_rest_get_metabox($post){
   foreach ($meta_boxes as $meta_box) {
       $meta_box = RW_Meta_Box::normalize($meta_box);
       if (!in_array($post->post_type, $meta_box['post_types'])) {
+        var_dump('SKIPPED '.$post->post_type );
+        var_dump(in_array($post->post_type, $meta_box['post_types']))
           continue;
       }
       foreach ($meta_box['fields'] as $field) {
