504df11b569565412916369d540cddb4ad844c72
franko <franko@akr.club>
2017-12-14 13:59:22 -0500

    update

--- edb-mb-rest-api.php
+++ edb-mb-rest-api.php
@@ -44,12 +44,15 @@ class MB_Rest_API {
       }
       
       foreach ( $meta_box->fields as $field ) {
-        var_dump('FIELD');
+        
         if ( empty( $field['id'] ) ) {
+          var_dump('FIELD EMPTY');
           continue;
         }
-        $field_value = rwmb_get_value( $field['id'] );
         
+        $field_value = rwmb_get_value( $field['id'] );
+        var_dump('FIELD '.$field['id']);
+        var_dump($field_value);
         /*
          * Make sure values of file/image fields are always indexed 0, 1, 2, ...
          * @link https://github.com/malfborger/mb-rest-api/commit/31aa8fa445c188e8a71ebff80027acbcaa0fd268
