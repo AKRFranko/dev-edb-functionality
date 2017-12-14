f618b03f74a5d4a3255c109f29f1601d4f9ab190
franko <franko@akr.club>
2017-12-14 15:17:12 -0500

    update

--- edb-mb-rest-api.php
+++ edb-mb-rest-api.php
@@ -51,8 +51,7 @@ class MB_Rest_API {
         }
 
         $field_value = rwmb_get_value( $field['id'] );
-        var_dump($field['id'] );
-        var_dump($field_value);
+        
         /*
          * Make sure values of file/image fields are always indexed 0, 1, 2, ...
          * @link https://github.com/malfborger/mb-rest-api/commit/31aa8fa445c188e8a71ebff80027acbcaa0fd268
@@ -60,6 +59,8 @@ class MB_Rest_API {
         if ( is_array( $field_value ) && in_array( $field['type'], array( 'media', 'file', 'file_upload', 'file_advanced', 'image', 'image_upload', 'image_advanced', 'plupload_image', 'thickbox_image' ), true ) ) {
           
           $field_value = array_values( $field_value );
+          var_dump($field['id'] );
+          var_dump($field_value);
         }
         
         $output[ $field['id'] ] = $field_value;
