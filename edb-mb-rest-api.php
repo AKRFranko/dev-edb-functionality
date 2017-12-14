aedef7cbfff89ccdcfbd336be03a6e6ea9c0f36a
franko <franko@akr.club>
2017-12-14 14:03:28 -0500

    update

--- edb-mb-rest-api.php
+++ edb-mb-rest-api.php
@@ -52,8 +52,8 @@ class MB_Rest_API {
         }
         
         $field_value = rwmb_get_value( $field['id'] );
-        var_dump('FIELD '.$field['id']);
-        var_dump($field_value);
+        // var_dump('FIELD '.$field['id']);
+        // var_dump($field_value);
         /*
          * Make sure values of file/image fields are always indexed 0, 1, 2, ...
          * @link https://github.com/malfborger/mb-rest-api/commit/31aa8fa445c188e8a71ebff80027acbcaa0fd268
